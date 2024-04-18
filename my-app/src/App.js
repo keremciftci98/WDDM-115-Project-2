0f4539b556ef3da7fd3fd7853817c33a73eb35e2
Kerem Ciftci <ckerem@sabanciuniv.edu>
2024-04-18 18:02:31 -0400

    Revert "jschange"
    This reverts commit 3fd80facc62edab3ceea3ecd67bff2741f7e7fb7.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import HomePage from './homepage';
-import MoviePage from './moviepage';
-import AboutPage from './aboutpage';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/movies">Movies</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-
-        <Switch>
-          <Route path="/movies" component={MoviePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/" component={HomePage} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
