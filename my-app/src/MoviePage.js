7a64dd1366784e7c6e226274f7957ecb3c25906d
Kerem Ciftci <ckerem@sabanciuniv.edu>
2024-04-18 01:25:55 -0400

    Created multiple pages for home, movie and rewards.

---
+++ my-app/src/MoviePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MoviePage() {
+  return (
+    <div>
+      <h2>Movie Page</h2>
+      <p>Welcome to the Movie Page!</p>
+    </div>
+  );
+}
+
+export default MoviePage;
