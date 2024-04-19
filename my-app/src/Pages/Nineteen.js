91ea477e4f5ad3a42cb91124809efe3b4c61a9fc
Miraclepossum <roxannavillamizaru@gmail.com>
2024-04-19 13:55:10 -0400

    Ninteen Structure
    Initial Structure

---
+++ my-app/src/Pages/Nineteen.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import { requests } from "../Request";
+import Row from "../components/Row";
+
+
+function Nineteen() {
+
+
+
+
+
+
+}
+
+export default Nineteen;
