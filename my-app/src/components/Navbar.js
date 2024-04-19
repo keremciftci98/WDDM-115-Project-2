646214c551d9c4855ce6dcd4449ec979391dfb56
ade-oni <126131639+ade-oni@users.noreply.github.com>
2024-04-19 12:21:17 -0400

    nav

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import { NavLink, Link } from "react-router-dom";
+import logo from '../assets/oscar_winners.svg';
+import { FaChevronDown, FaInstagram, FaSpotify } from "react-icons/fa6";
+
+function NavBar() {
+
+    return (
+
+
+
+        );
+    }
+    
+    export default NavBar;
+    
