c97edcc05db0acf50b07d14575e48556f379458b
ade-oni <126131639+ade-oni@users.noreply.github.com>
2024-04-19 16:08:01 -0400

    hero

--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -2,7 +2,16 @@ import trophy from '../assets/trophy.png';
 
 function Hero() {
     return (
-     
+        <section className="Hero h-dvh">
+            <div className="container mx-auto">
+                <div className='flex justify-center items-center h-dvh w-3/4 mx-auto'>
+                    <div className='w-1/2'>
+                        <img src={trophy} alt="trophy" />
+                    </div>
+                    <div className='text-white rammetto-one text-5xl leading-[81px]'>Discover the winners and nominees of The Academy Awards</div>
+                </div>
+            </div>
+        </section>
     );
 }
 
